0443689b6eadac5997b49a441e8e7f81819e419b
Santiago <santigilma@gmail.com>
2022-09-20 18:18:08 -0400

    Changing file structure

--- client/index.js
+++ client/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './App.js';
 import styles from './App.scss';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
 
-ReactDOM.render(<App /> , document.getElementById('root'));
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
