60e0d31d9b70b99ad16efd1dd9a4b8b3f540a72c
Santiago <santigilma@gmail.com>
2022-09-21 16:42:59 -0400

    Added client-side calls to server REST API for CRUD functions. Added logout functionality. Correctly rendering tasks

--- client/components/Api.js
+++ client/components/Api.js
@@ -39,7 +39,7 @@ Api.signup = async (payload) => {
 
 Api.getAllTasks = async (payload) => {
   console.log(payload);
-  const url = `/task/task/${payload.username}`;
+  const url = `/task/${payload.username}`;
   const option = {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
@@ -48,10 +48,78 @@ Api.getAllTasks = async (payload) => {
   const response = await fetch(url, option)
     .then((response) => response.json())
     .then((data) => {
-      if (data.length) return data.filter((task) => task.category === 'todo');
+      console.log('In getAllTasks. This is the data:');
+      console.log(data);
+      if (data.length > 0)
+        return data.filter((task) => task.progress === payload.progress);
+      console.log('About to log null');
       return null;
     })
-    .catch((err) => console.log(`Error getting all ${payload.category} tasks`));
+    .catch((err) => console.log(`Error getting all ${payload.progress} tasks`));
+
+  return response;
+};
+
+//CREATE TASK API
+Api.createTask = async (payload) => {
+  const url = '/task';
+  const option = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload),
+  };
+  const response = await fetch(url, option)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((err) => console.log('Error with API.createTask'));
+  return response;
+};
+
+//MOVE TASK API
+Api.moveTask = async (payload) => {
+  const url = '/task/move';
+  const option = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload),
+  };
+  const response = await fetch(url, option)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((err) => console.log('Error with Api.moveTask'));
+  return response;
+};
+
+//UPDATE TASK API
+Api.updateTask = async (payload) => {
+  const url = '/task/update';
+  const option = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload),
+  };
+  const response = await fetch(url, option)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((err) => console.log('Error with Api.editTask'));
+  return response;
+};
+
+//DELETE TASK API
+Api.deleteTask = async (payload) => {
+  console.log(payload);
+  const url = '/task';
+  const option = {
+    method: 'DELETE',
+    body: JSON.stringify(payload),
+    headers: { 'Content-Type': 'application/json' },
+  };
+  //fetch api call
+  const response = await fetch(url, option)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((err) => console.log('Error with API.deleteTask'));
+  return response;
 };
 
 export default Api;
