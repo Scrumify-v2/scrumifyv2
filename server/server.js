99598dcf524da36bbcc0000a0ef3309a1d0030f9
Santiago <santigilma@gmail.com>
2022-09-21 14:11:52 -0400

    Changes to todo.js and api.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.static(path.resolve(__dirname, '')));
 
 // app.use('/projects', projectRouter);
 
-// app.use('user/task', taskRouter);
+app.use('/task', taskRouter);
 app.use('/user', userRouter);
 
 app.get('/', (req, res) => {
