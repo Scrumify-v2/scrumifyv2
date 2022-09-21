2938c6770d6cae37e2900f3c44397e40ce05bb1b
Kris Magat <kmag.dev@gmail.com>
2022-09-21 08:45:17 -0700

    update

--- server/models/models.js
+++ server/models/models.js
@@ -1,31 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const DB_URI = 'mongodb+srv://adrian:themostsecurepassword@cluster0.wqkokkj.mongodb.net/?retryWrites=true&w=majority';
+const DB_URI =
+  'mongodb+srv://adrian:themostsecurepassword@cluster0.wqkokkj.mongodb.net/?retryWrites=true&w=majority';
 
-
-mongoose.connect(DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: 'SCRUM_Project'
-})
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'SCRUM_Project',
+  })
   .then(() => console.log('Connected to Mongo DB'))
-  .catch(err => console.log(err));
-
+  .catch((err) => console.log(err));
 
 // minimize: false overrides default behavior of not saving empty arrays/objects
-const userSchema = new Schema({
-  username: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  tasks: [{type: Schema.Types.ObjectId, ref: 'Task'}]
-}, {minimize: false}
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
+  },
+  { minimize: false }
 );
 
 const User = mongoose.model('user', userSchema);
 
 const sessionSchema = new Schema({
   cookieId: { type: String, required: true, unique: true },
-  createdAt: { type: Date, expires: 30, default: Date.now }
+  createdAt: { type: Date, expires: 30, default: Date.now },
 });
 
 const Session = mongoose.model('session', sessionSchema);
@@ -44,10 +46,10 @@ const Project = mongoose.model('project', projectSchema);
  */
 
 const taskSchema = new Schema({
-  user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-  taskname: {type: String, required: true},
-  content: {type: String, required: true},
-  progress: {type: String, required: true}
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  taskname: { type: String, required: true },
+  content: { type: String, required: true },
+  progress: { type: String, required: true },
 });
 
 const Task = mongoose.model('task', taskSchema);
@@ -55,5 +57,5 @@ const Task = mongoose.model('task', taskSchema);
 module.exports = {
   User,
   Session,
-  Task
+  Task,
 };
