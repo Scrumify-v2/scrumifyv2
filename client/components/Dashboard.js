bd05c6df40f0853c5add8d5aa408d3bad2dc4fe7
Kris Magat <kmag.dev@gmail.com>
2022-09-21 09:28:10 -0700

    updated dashboard

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -8,25 +8,22 @@ import Completed from './Completed.js';
 export default function Dashboard() {
   const navigate = useNavigate();
 
-  // const handleClick = () => {
-  //   return navigate('/login');
-  //   <div>
-  //   <div>Dashboard</div>
-  //   <button type='button' onClick={() => handleClick()}>
-  //     Log In
-  //   </button>
-  // </div>
-  // };
+  const handleClick = () => {
+    return navigate('/login');
+  };
 
   return (
-    <>
+    <div>
       <h2>Scrumify v2.0</h2>
+      <button type='button' onClick={() => handleClick()}>
+        Log In
+      </button>
       <Header />
       <section className='mainContent'>
         <Todo />
         <Progress />
         <Completed />
       </section>
-    </>
+    </div>
   );
 }
