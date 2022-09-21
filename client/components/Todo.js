7628ccaf7ffd534d701967bd692057e6a0d4f6b0
Santiago <santigilma@gmail.com>
2022-09-21 12:25:33 -0400

    changed styling

--- client/components/Todo.js
+++ client/components/Todo.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Task from './Task';
 
 export default function Todo() {
   return (
     <section className='column'>
       <h2>Todo</h2>
+      <Task />
     </section>
   );
 }
